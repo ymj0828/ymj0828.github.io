de19b25f8f7ebaa3350a7742ff44d0648c9e12ca
ymj0828 <ymj369@nate.com>
2023-03-09 23:48:18 +0900

    포폴페이지

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(() => {
+
+      $('.graph_wrap:eq(0) span').stop().animate({width:'90%'}, 500);
+      $('.graph_wrap:eq(1) span').stop().animate({width:'90%'}, 500);
+      $('.graph_wrap:eq(2) span').stop().animate({width:'75%'}, 500);
+
+});
